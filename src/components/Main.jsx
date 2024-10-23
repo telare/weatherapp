09d7413ac3eba86300443bd83d681bedbd2483d9
telare <andrewzhubinskiy21@gmail.com>
2024-10-23 21:17:02 +0300

    main 0.0

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,25 @@
 function Main(){
     return (
-        <>
-        </>
+        <div className="w-full h-screen bg-slate-600">
+            <div className="flex">
+                <div>
+                    <h2>Sunny</h2>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <h2>29 C</h2>
+                    {/* Wind table */}
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+            {/* Reccomendation for wearing */}
+            <div></div>
+        </div>
     )
 }
 export default Main
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import mainIcon from '../assets/main-icon.png';
 
 function Nav() {
     return (
-        <>
+        
             <div className="bg-black w-full h-12 flex text-slate-200 justify-around" >
                 <img src={mainIcon} alt="Main Icon" className=""/>
                 <h1 className='mt-0 text-big'>React Weather</h1>
@@ -12,7 +12,7 @@ function Nav() {
                     <Link to="https://github.com/telare/weatherapp">GitHub</Link>
                 </div>
           </div>
-        </>
+       
     )
 }
 export default Nav
